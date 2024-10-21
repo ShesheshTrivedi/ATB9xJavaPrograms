242f1897d1872309259df62c0ecc5fec6c6aeab8
Sheshesh Trivedi <trivedisheshesh.gmail.com>
2024-10-21 06:47:48 +0530

    15102024

---
+++ src/Oct/ex21102024_ForLoop/Lab08.java
@@ -0,0 +1,10 @@
+package Oct.ex21102024_ForLoop;
+
+public class Lab08 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++){
+            System.out.println(i);
+
+        }
+    }
+}
---
+++ src/Oct/ex21102024_ForLoop/Lab09.java
@@ -0,0 +1,10 @@
+package Oct.ex21102024_ForLoop;
+
+public class Lab09 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 1 ; i++){
+            System.out.println("End Process");
+
+        }
+    }
+}
